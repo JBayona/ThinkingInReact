aec844bfa2410a84f06f0bebca91207a5e1667ab
JBayona <jordavids_22@hotmail.com>
2017-03-21 23:04:45 -0600

    Add ReactJS

--- server.js
+++ server.js
@@ -3,11 +3,9 @@ const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config.js');
 const app = express();
- 
+
 const compiler = webpack(webpackConfig);
- 
-app.use(express.static(__dirname + '/www'));
- 
+
 app.use(webpackDevMiddleware(compiler, {
   hot: true,
   filename: 'bundle.js',
@@ -17,7 +15,9 @@ app.use(webpackDevMiddleware(compiler, {
   },
   historyApiFallback: true,
 }));
- 
+
+app.use(express.static(__dirname + '/www'));
+
 const server = app.listen(3000, function() {
   const host = server.address().address;
   const port = server.address().port;
