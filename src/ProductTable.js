547248e906f40083f947393aa01a00eb718674cc
JBayona <jordavids_22@hotmail.com>
2017-03-23 21:42:55 -0600

    Logic implemented

--- src/ProductTable.js
+++ src/ProductTable.js
@@ -8,11 +8,11 @@ class ProductTable extends React.Component{
 		var lastCategory = null;
 
 		this.props.products.forEach(product => {
-			if(product.category != lastCategory){
-				rows.push(<ProductCategoryRow product = {product} key = {product.category}/>);
-				rows.push(<ProductRow product = {product} key = {product.name}/>);
-				lastCategory = product.category;
+			if(product.category !== lastCategory){
+				rows.push(<ProductCategoryRow category = {product.category} key = {product.category}/>);
 			}
+			rows.push(<ProductRow product = {product} key = {product.name}/>);
+			lastCategory = product.category;
 		});
 		return (
 			<table>
